879d1245b1f2b51eddea2c293b2d90cc67b8519f
JorgeYael-CB <jorge.yael333@gmail.com>
2024-07-26 21:34:39 -0600

    Rutas y navbar agregado

--- src/presentation/components/NavBar.tsx
+++ src/presentation/components/NavBar.tsx
@@ -9,12 +9,12 @@ interface Props {
 export const NavBar = ( {routes}:Props ) => {
   return (
     <>
-      <div className="flex justify-around py-4 bg-black">
-        <nav className="flex gap-4">
+      <div className="flex justify-around py-3 bg-black">
+        <nav className="flex gap-6">
           {
             routes.map( r => (
               <NavLink
-                className={ (active) => `font-semibold text-base ${ active.isActive ? 'text-white underline' : 'text-gray-300' }` } to={r.route}>{r.title}
+                className={ (active) => `font-semibold rounded-sm transition-colors px-3 py-1 text-base ${ active.isActive ? 'bg-white text-black' : 'text-white' }` } to={r.route}>{r.title}
               </NavLink>
             ))
           }
@@ -22,9 +22,9 @@ export const NavBar = ( {routes}:Props ) => {
 
         <nav className="flex gap-3">
           <NavLink
-            className={ (active) => `font-semibold text-base ${ active.isActive ? 'text-white underline' : 'text-gray-300' }` } to='/auth/login'>Login</NavLink>
+            className={ (active) => `font-semibold rounded-sm transition-colors px-3 py-1 text-base ${ active.isActive ? 'bg-white text-black' : 'text-white' }` } to='/auth/login'>Login</NavLink>
           <NavLink
-            className={ (active) => `font-semibold text-base ${ active.isActive ? 'text-white underline' : 'text-gray-300' }` } to='/auth/register'>Register</NavLink>
+            className={ (active) => `font-semibold rounded-sm transition-colors px-3 py-1 text-base ${ active.isActive ? 'bg-white text-black' : 'text-white' }` } to='/auth/register'>Register</NavLink>
         </nav>
       </div>
     </>
